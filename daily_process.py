76bcd2a054a7ea533e6eea46207faab831061138
Galsor <meilliez.antoine@gmail.com>
2020-04-13 14:59:44 +0200

    Initial commit

---
+++ daily_process.py
@@ -0,0 +1,12 @@
+
+def main():
+    #TODO:
+    # get value of the day.
+    # computute indicators
+    # get model from joblib
+    # predict
+    # notify people by mail.
+
+
+if __name__ == '__main__':
+    main()
